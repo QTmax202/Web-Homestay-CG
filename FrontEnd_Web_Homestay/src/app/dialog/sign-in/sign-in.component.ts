de1ef76897cb14336fcc625031a411cf8dc9fcd9
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2022-04-10 00:26:00 +0700

    00/00 10/04/2022

--- FrontEnd_Web_Homestay/src/app/dialog/sign-in/sign-in.component.ts
+++ FrontEnd_Web_Homestay/src/app/dialog/sign-in/sign-in.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {first} from "rxjs";
 import {MatDialog} from "@angular/material/dialog";
 import {SignUpComponent} from "../sign-up/sign-up.component";
+import {AuthenticationService} from "../../service/authentication.service";
 
 @Component({
   selector: 'app-sign-in',
@@ -9,9 +13,25 @@ import {SignUpComponent} from "../sign-up/sign-up.component";
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private dialog: MatDialog) { }
+  loginForm: FormGroup = new FormGroup({
+    gmail: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required, Validators.minLength(5)])
+  });
+
+  accountUrl?: string;
+  error = '';
+  hide = true;
+
+  constructor(private dialog: MatDialog,
+              private activatedRoute: ActivatedRoute,
+              private router: Router,
+              private authenticationService: AuthenticationService) {
+    console.log(this.authenticationService.currentUserValue);
+  }
 
   ngOnInit(): void {
+    const {returnUrl: returnUrl} = this.activatedRoute.snapshot.queryParams;
+    this.accountUrl = returnUrl || '/';
   }
 
   openSignUp() {
@@ -19,5 +39,37 @@ export class SignInComponent implements OnInit {
     this.dialog.open(SignUpComponent, {
       width: '30%',
     });
+  }
+
+  login() {
+    this.authenticationService.login(this.loginForm.value.gmail, this.loginForm.value.password)
+      .pipe(first())
+      .subscribe(
+        data => {
+          if (data.status == 202){
+            // @ts-ignore
+            document.getElementById("error-form-login").innerHTML = 'Tài khoản của bạn chưa có trong hệ thống!'
+            localStorage.removeItem('ACCESS_TOKEN');
+          } else if (localStorage.getItem('currentAccount') == "unverified account"){
+            // @ts-ignore
+            document.getElementById("error-form-login").innerHTML = 'Tài khoản của bạn đã bị khoá hoặc sai mật khẩu!'
+            localStorage.removeItem('ACCESS_TOKEN');
+          } else if (data.token != undefined){
+            localStorage.setItem('ACCESS_TOKEN', data.token);
+              // localStorage.setItem('ROLE', data.roles[0].authority);
+              // localStorage.setItem('EMAIL', data.email);
+              // this.router.navigate([this.accountUrl]);
+            alert('We')
+              this.dialog.closeAll();
+          }
+        },
+        error => {
+          // @ts-ignore
+          document.getElementById("error-form-login").innerHTML = 'Tài khoản của bạn đã bị khoá hoặc sai mật khẩu!'
+          localStorage.removeItem('currentUser');
+          // localStorage.removeItem('ROLE');
+          localStorage.removeItem('ACCESS_TOKEN');
+          // localStorage.removeItem('EMAIL');
+        });
   }
 }
---
+++ FrontEnd_Web_Homestay/src/app/service/authentication.service.ts
@@ -0,0 +1,49 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {BehaviorSubject, map, Observable} from "rxjs";
+import {AccToken} from "../models/acc-token";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private currentUserSubject: BehaviorSubject<AccToken>;
+  public currentUser: Observable<AccToken>;
+  update = new EventEmitter<string>();
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<AccToken>(JSON.parse(<any>localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): AccToken {
+    return this.currentUserSubject.value;
+  }
+
+  login(gmail: string, password: string) {
+    return this.http.post<any>('http://localhost:8080/api/auth/sign-in', {gmail, password})
+      .pipe(map(account => {
+        if (account.status == 200) {
+          if (account.error.text == "unverified account") {
+            localStorage.setItem('currentAccount', "unverified account");
+          } else {
+            localStorage.setItem('currentAccount', JSON.stringify(account));
+            this.currentUserSubject.next(account);
+            this.update.emit('login');
+          }
+        } else if (account.status == 202) {
+          localStorage.setItem('currentAccount', "not account");
+        }
+        return account;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem('currentAccount');
+    // localStorage.removeItem('ROLE');
+    // localStorage.removeItem('ACCESS_TOKEN');
+    // localStorage.removeItem('EMAIL');
+    // this.currentUserSubject.next(null);
+  }
+}
