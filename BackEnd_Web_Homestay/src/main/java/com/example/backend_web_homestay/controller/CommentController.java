779af94b549c3364cc6a05957e8cb9d80c3a8db1
hongnamz1211 <hongnamz1211@gmail.com>
2022-04-11 10:50:44 +0700

    add controller comment

--- BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/controller/CommentController.java
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/controller/CommentController.java
@@ -1,2 +1,26 @@
-package com.example.backend_web_homestay.controller;public class CommentController {
+package com.example.backend_web_homestay.controller;
+
+import com.example.backend_web_homestay.model.Comment;
+import com.example.backend_web_homestay.service.Comment.ICommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comment")
+@CrossOrigin("*")
+public class CommentController {
+
+    @Autowired
+    private ICommentService commentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCommentsByHomestayId(@PathVariable long id) {
+        Iterable<Comment> comments = commentService.getCommentsByHomestayId(id);
+        if (!comments.iterator().hasNext()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
 }
--- BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/service/Comment/ICommentService.java
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/service/Comment/ICommentService.java
@@ -1,2 +1,8 @@
-package com.example.backend_web_homestay.service.Comment;public interface ICommentService {
+package com.example.backend_web_homestay.service.Comment;
+
+import com.example.backend_web_homestay.model.Comment;
+import com.example.backend_web_homestay.service.IGeneralService;
+
+public interface ICommentService extends IGeneralService<Comment> {
+    Iterable<Comment> getCommentsByHomestayId(long id);
 }
