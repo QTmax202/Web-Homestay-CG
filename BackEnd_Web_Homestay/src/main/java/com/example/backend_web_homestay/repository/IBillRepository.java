0f2b905212b6bad7f041d89a4e573a1a614f720d
QTmax202 <89342545+QTmax202@users.noreply.github.com>
2022-04-14 23:32:24 +0700

    11/32 14/04/2022

--- BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/repository/IBillRepository.java
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/repository/IBillRepository.java
@@ -2,8 +2,18 @@ package com.example.backend_web_homestay.repository;
 
 import com.example.backend_web_homestay.model.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IBillRepository extends JpaRepository<Bill, Long> {
+    Iterable<Bill> getBillByHomestay_Id(Long id);
+
+    @Query(value = "select * from bill b \n" +
+            "join status_homestay s on s.id = b.status_homestay_id\n" +
+            "having (s.id != 2 or s.id =  3) and b.homestay_id = ?;"
+            , nativeQuery = true)
+    List<Bill> findAllBillByHomestay_Status(Long id);
 }
---
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/service/StatusHomestay/IStatusHomestayService.java
@@ -0,0 +1,7 @@
+package com.example.backend_web_homestay.service.StatusHomestay;
+
+import com.example.backend_web_homestay.model.StatusHomestay;
+import com.example.backend_web_homestay.service.IGeneralService;
+
+public interface IStatusHomestayService extends IGeneralService<StatusHomestay> {
+}
