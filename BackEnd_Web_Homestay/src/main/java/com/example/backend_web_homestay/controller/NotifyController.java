8b79a885807c2ea8ffaef2676cbcd24d6af10849
hongnamz1211 <hongnamz1211@gmail.com>
2022-04-19 23:02:41 +0700

    notify be

--- BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/controller/NotifyController.java
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/controller/NotifyController.java
@@ -1,14 +1,15 @@
 package com.example.backend_web_homestay.controller;
 
+import com.example.backend_web_homestay.model.Notify;
 import com.example.backend_web_homestay.repository.INotifyRepository;
 import com.example.backend_web_homestay.service.Notify.INotifyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/bill")
+@RequestMapping("/api/notify")
 @CrossOrigin("*")
 public class NotifyController {
     @Autowired
@@ -16,4 +17,13 @@ public class NotifyController {
 
     @Autowired
     private INotifyService notifyService;
+
+    @GetMapping("/account/{id}")
+    private ResponseEntity<?> getNotifyByAccountDesc(@PathVariable long id) {
+        Iterable<Notify> notifies = notifyService.getNotifyByAccountDesc(id);
+        if (!notifies.iterator().hasNext()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/repository/INotifyRepository.java
+++ BackEnd_Web_Homestay/src/main/java/com/example/backend_web_homestay/repository/INotifyRepository.java
@@ -2,9 +2,13 @@ package com.example.backend_web_homestay.repository;
 
 import com.example.backend_web_homestay.model.Notify;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface INotifyRepository extends JpaRepository<Notify, Long> {
-    Iterable<Notify> getNotifyByAccount_Id(Long id);
+    @Query(value = "select * from notify \n" +
+            "where n.account_id = :id \n" +
+            "order by n.id desc;", nativeQuery = true)
+    Iterable<Notify> getNotifyByAccountDesc(Long id);
 }
